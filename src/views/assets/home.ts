ba57f7ae081d95de1070a16dd0dcda87399b4af4
Tuan Binh Tran <tuanbinh.tran@icloud.com>
2020-01-15 21:55:21 +0100

    Add styling

--- src/views/assets/home.ts
+++ src/views/assets/home.ts
@@ -3,21 +3,38 @@ import { Product } from '../../products/product';
 
 console.log('home.ts')
 
-var productsElement = document.getElementsByClassName('pure-g')[0];
-var template = nunjucks.compile(`
-{% for product in products %}
-    <div class="pure-u-1-2 pure-u-lg-1-3 m-box" >
-        <img class="pure-img" src="./assets/{{  product.imageName  }}" />
-        <p>{{ product.productName }}</p>
-    </div>
-{% endfor %}
-`);
+var productsGridElement = document.getElementById('products-grid');
+// var template = nunjucks.compile(`
+// {% for product in products %}
+//     <div class="pure-u-1-2 pure-u-lg-1-3 m-box" >
+//         <img class="pure-img" src="./assets/{{  product.imageName  }}" />
+//         <p>{{ product.productName }}</p>
+//         <p class="normalPriceTag">{{ product.normalPrice }}</p>
+//         <p class="specialOfferTag">{{ product.specialOffer }}</p>
+//     </div>
+// {% endfor %}
+// `);
 
-var products =  fetch('api/products')
-    .then(products => products.json())
-    .then((products: Product[]) => products)
-    .then(products => {
-        console.log(products);
-        productsElement.innerHTML += template.render({ products: products });
-    });
+
+fetch('api/products')
+    .then((products) => products.json())
+    .then((products: Product[]) => { products.forEach(renderProduct) });
+
+function renderProduct(product: Product) {
+    productsGridElement.innerHTML +=
+        `
+        <div class="pure-u-1-2 pure-u-lg-1-3 m-box" >
+            <div class="product">
+                <div class="thumbnail">
+                    <img class="pure-img" src="./assets/${product.imageName}"/>
+                </div>
+                <div class="content">
+                    <p>${product.productName}</p>
+                    <p class="normalPriceTag">${product.normalPrice.toFixed(2)}</p>
+                    <p class="specialOfferTag">${product.specialOffer.toFixed(2)}</p>
+                </div>
+            </div>
+        </div>
+        `
+}
 
